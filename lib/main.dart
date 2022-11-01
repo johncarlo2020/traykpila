f10a06e4ae7d672ee1f232a72b44aafc04770c96
kenzo0906 <104350367+kenzo0906@users.noreply.github.com>
2022-11-01 19:04:43 +0800

    added welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:traykpila/screens/loading.dart';
+import 'package:traykpila/screens/welcome.dart';
 
 void main() {
   runApp(const App());
@@ -12,7 +13,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Loading(),
+      home: Welcome(),
 
     );
   }
